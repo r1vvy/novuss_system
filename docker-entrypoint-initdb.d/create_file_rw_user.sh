deb14fd56549bbcacad79a1a0b7fe22b9217e252
r1vvy <s20seflemil@vea.lv>
2023-05-15 10:48:39 +0300

    db webapp initial commit

---
+++ docker-entrypoint-initdb.d/create_file_rw_user.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Grant read/write permissions to the var/uploads folder
+#useradd -m -s /bin/bash $FILE_USER
+#passwd $FILE_USER
+#chown $FILE_USER:$FILE_USER $FILE_UPLOADS_PATH
+#chmod 770 $FILE_UPLOADS_PATH
